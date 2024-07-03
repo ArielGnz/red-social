fa105f2783d35ebe95f5a5063fea9c26c0ae1519
ArielGnz <arielgon2206@gmail.com>
2024-07-03 20:09:55 -0300

    user controller

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -5,11 +5,20 @@ const register = (req, res) => {
     
     let params = req.body;
 
-    if(params.name && params.email && params.password )
+    if(!params.name || !params.email || !params.password || !params.surname || !params.nick ){
+        return res.status(400).json({
+            status: "error",
+            message: "Faltan Datos"
+        })
+    }
+
+    let userSave = new User(params)
 
 
     return res.status(200).json({
-        message: "Accion de registro"
+        status: "success",
+        message: "Accion de registro",
+        userSave
     });
     
 }
