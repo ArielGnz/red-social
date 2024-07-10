15ea5ffa85ff8eae465d24e6a804eb8f696a0f6d
ArielGnz <arielgon2206@gmail.com>
2024-07-10 19:48:49 -0300

    Login

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,68 +1,3 @@
-// const User = require("../models/user");
-// const bcrypt = require("bcrypt");
-
-
-// const register = async(req, res) => {
-    
-//     // Registro de usuario
-//     let params = req.body;
-
-//     // Comprobacion de que llegan los parametros
-//     if(!params.name || !params.email || !params.password || !params.surname || !params.nick ){
-//         return res.status(400).json({
-//             status: "error",
-//             message: "Faltan Datos"
-//         })
-//     }
-
-//     User.find({
-//         $or: [
-//         {email: params.email.toLowerCase()},
-//         {nick: params.nick.toLowerCase()}
-//         ]
-
-//     }).exec(async (error, users) => {
-
-//             if (error) return res.status(500).json({status:"error", message:"Ya existe Emial"})
-        
-//             if(users && users.length >=1){
-//                 return res.status(200).send({
-//                     status: "success",
-//                     message: "El usuario ya esxiste"
-//                 });
-//             }
-
-
-//             //  cifrar contraseña
-//             let pwd = await bcrypt.hash(params.password, 10);
-//             params.password = pwd;
-
-//             // Creo el objeto de usuario
-//             let userSave = new User(params);
-
-//             //  Guardar usuario en DB
-//             userSave.save((err, userStored) => {
-//                 if (err || !userStored) return res.status(500).send({status:"error", message:"Error al guardar el usuario"})
-            
-//                  // Devuelvo el resultado
-//                 return res.status(200).json({
-//                     status: "success",
-//                     message: "Accion de registro",
-//                     userSave
-//                 });
-//             });
-           
-//         })
-
-   
-    
-   
-    
-// }
-
-// module.exports = {
-//     register
-// }
 
 const User = require("../models/user");
 const bcrypt = require("bcrypt");
@@ -116,6 +51,35 @@ const register = async (req, res) => {
             message: error.message
         });
     }
+}
+
+const login = (req, res) => {
+    // Recoger parametros
+    let params = req.body;
+
+    if(!params.email || !params.password){
+        return res.status(400).send({
+            status: "error",
+            message: "Faltan datos por enviar"
+        })
+    }
+
+    // Buscar en la DB si existe usuario
+    User.findOne({email: params.email}).exec((error, user) => {
+        if(error || !user) return res.status(404).send({status: "error", message:"No existe el usuario"});
+
+        // Comprobar contraseña
+        const pwd = bcrypt.compareSync(params.password, user.password);
+
+        if(!pwd) {
+            return res.status(400).send({
+                status: "error",
+                mesagge: "Error en datos de ingreso"
+            })
+        }
+
+        // Devolver datos de usuario
+    })
 }
 
 module.exports = {
