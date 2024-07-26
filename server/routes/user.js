06c5b7c2ffd7f0d4aeec3572361704743532b37f
ArielGnz <arielgon2206@gmail.com>
2024-07-26 19:45:56 -0300

    files

--- server/routes/user.js
+++ server/routes/user.js
@@ -3,7 +3,18 @@ const express = require("express");
 const multer = require("multer");
 const router = express.Router();
 const userController = require("../controllers/user");
-const check = require("../middlewares/auth")
+const check = require("../middlewares/auth");
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, "./uploads/avatars/")
+    },
+    filename: (req, file, cb) => {
+        cb(null, "avatar-"+Date.now()+"-"+file.originalname);
+    }
+});
+
+const uploads = multer({storage});
 
 router.get("/prueba-usuario", check.auth, userController.pruebaUser);
 router.post("/register", userController.register);
@@ -11,6 +22,6 @@ router.post("/login", userController.login);
 router.get("/profile/:id", check.auth, userController.profile);
 router.get("/list/:page?", check.auth, userController.list);
 router.put("/update", check.auth, userController.update);
-router.post("/upload",check.auth, userController.upload);
+router.post("/upload",[check.auth, uploads.single("file0")], userController.upload);
 
 module.exports = router;
