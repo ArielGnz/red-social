33a9e04fb5abbea6cb4f2e57f3c5c27a745d1c62
ArielGnz <arielgon2206@gmail.com>
2024-08-01 20:36:43 -0300

    follows

--- server/controllers/follow.js
+++ server/controllers/follow.js
@@ -2,12 +2,16 @@ const Follow = require("../models/follow");
 const User = require("../models/user");
 
 const pruebaFollow = (req, res) => {
-
-    document.querySelector()
-
     return res.status(200).send({
         message: "Mensaje enviado desde: controllers/follow.js"
     });
+}
+
+const save = (req, res) => {
+    return res.status(200).send({
+        status: "success",
+        message: "siguiendo"
+    })
 }
 
 module.exports = {
