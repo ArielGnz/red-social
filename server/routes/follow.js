1e2a672b8288d9e7769ca05f490c5b664f056c55
ArielGnz <arielgon2206@gmail.com>
2024-08-01 19:50:53 -0300

    follow routes

---
+++ server/routes/follow.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const router = express.Router();
+const FollowContoller = require("../controllers/follow");
+const check = require("../middlewares/auth");
+
+
+router.get("/prueba-follow", FollowContoller.pruebaFollow);
+router.post("/save", check.auth, FollowContoller.save);
