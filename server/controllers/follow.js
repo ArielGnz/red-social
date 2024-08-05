74e98beb85f012dc5c44abbc8fe4db8971e2758d
ArielGnz <arielgon2206@gmail.com>
2024-08-05 19:48:51 -0300

    follow stored

--- server/controllers/follow.js
+++ server/controllers/follow.js
@@ -8,6 +8,29 @@ const pruebaFollow = (req, res) => {
 }
 
 const save = (req, res) => {
+
+    // Recoger datos
+    const params = req.body;
+
+    // Obtener id del usuario identificado
+    const identity = req.user;
+
+    // Crear objeto con el modelo follow
+    let userToFollow = new Follow({
+        user: identity.id,
+        followed: params.followed
+    });
+
+    // Guardar objeto en BBDD
+    userToFollow.save((error, followStored) => {
+        if(error || !followStored){
+            return res.status(500).send({
+                status: "Error",
+                message: "No se ha podido seguir al usuario"
+            });
+        }
+    })
+
     return res.status(200).send({
         status: "success",
         message: "siguiendo"
