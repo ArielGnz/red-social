5d68d0f16c347194277a66698622799db4bed673
ArielGon <arielgon2206@gmail.com>
2024-08-26 20:20:51 -0300

    Routes

---
+++ client/src/router/Routing.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Routes, Route, BrowserRouter, Navigate} from 'react-router-dom';
+
+export default function Routing() {
+  return (
+    <BrowserRouter>
+        <Routes>
+            
+        </Routes>
+    </BrowserRouter>
+  )
+}
