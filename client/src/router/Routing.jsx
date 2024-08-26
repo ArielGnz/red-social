c5792572b91845fe46cde056f1766622cfcb1a86
ArielGon <arielgon2206@gmail.com>
2024-08-26 20:27:18 -0300

    layout public

--- client/src/router/Routing.jsx
+++ client/src/router/Routing.jsx
@@ -5,7 +5,9 @@ export default function Routing() {
   return (
     <BrowserRouter>
         <Routes>
-            
+            <Route path='/'>
+
+            </Route>
         </Routes>
     </BrowserRouter>
   )
