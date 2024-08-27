647d8c494487846d0bffa1ebc72915fb79eb15bf
ArielGnz <arielgon2206@gmail.com>
2024-08-27 19:39:02 -0300

    Login jsx

--- client/src/router/Routing.jsx
+++ client/src/router/Routing.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import {Routes, Route, BrowserRouter, Navigate} from 'react-router-dom';
+import PublicLayout from '../components/layout/public/PublicLayout';
 
 export default function Routing() {
   return (
     <BrowserRouter>
         <Routes>
-            <Route path='/'>
+            <Route path='/' element={<PublicLayout/>}>
 
             </Route>
         </Routes>
