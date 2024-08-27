c69a22fc0693110d7fd7cfa45fe2ffbd172afacd
ArielGnz <arielgon2206@gmail.com>
2024-08-27 19:44:19 -0300

    routing

--- client/src/router/Routing.jsx
+++ client/src/router/Routing.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import {Routes, Route, BrowserRouter, Navigate} from 'react-router-dom';
 import PublicLayout from '../components/layout/public/PublicLayout';
+import Register from '../components/user/Register';
+import { Login } from '../components/user/Login';
 
 export default function Routing() {
   return (
     <BrowserRouter>
         <Routes>
             <Route path='/' element={<PublicLayout/>}>
-
+              <Route index element={<Login />} />
+              <Route path='login' element={<Login />} />
+              <Route path='registro' element={<Register />} />
             </Route>
         </Routes>
     </BrowserRouter>
