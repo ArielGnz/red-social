61d84319711d1f0d780809143074fafb9b342399
ArielGnz <arielgon2206@gmail.com>
2024-08-29 18:43:15 -0300

    useForm

--- client/src/components/user/Register.jsx
+++ client/src/components/user/Register.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
+import { useForm } from '../../hooks/useForm'
 
 export default function Register() {
+
+    const {form, changed} = useForm({})
+
     return (
         <>
             <header className="content__header content__header--public">
@@ -12,27 +16,27 @@ export default function Register() {
                     
                     <div className='form-group'>
                         <label htmlFor="name">Nombre</label>
-                        <input type="text" name='name' />
+                        <input type="text" name='name' onChange={changed}/>
                     </div>
 
                     <div className='form-group'>
                         <label htmlFor="surnname">Apellido</label>
-                        <input type="text" name='surnname' />
+                        <input type="text" name='surnname' onChange={changed}/>
                     </div>
 
                     <div className='form-group'>
                         <label htmlFor="nick">Nick</label>
-                        <input type="text" name='nick' />
+                        <input type="text" name='nick' onChange={changed}/>
                     </div>
 
                     <div className='form-group'>
                         <label htmlFor="email">Email</label>
-                        <input type="email" name='email' />
+                        <input type="email" name='email' onChange={changed}/>
                     </div>
 
                     <div className='form-group'>
                         <label htmlFor="password">Contraseña</label>
-                        <input type="password" name='password' />
+                        <input type="password" name='password' onChange={changed}/>
                     </div>
 
                     <input type="submit" value="Registrate" className='btn btn-success' />
