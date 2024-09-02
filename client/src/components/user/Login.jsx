659930a178d0cc2bb4821d958c95acc191d1c32c
ArielGnz <arielgon2206@gmail.com>
2024-09-02 18:17:28 -0300

    Login form

--- client/src/components/user/Login.jsx
+++ client/src/components/user/Login.jsx
@@ -1,6 +1,28 @@
 import React from 'react'
+import { useForm } from '../../hooks/useForm'
+import { Global } from '../../helpers/Global';
 
 export const Login = () => {
+
+  const {form, changed} = useForm({});
+
+  const loginUser = async (e) =>{
+    e.preventDefaukt();
+
+    let userToLogin = form;
+    console.log(form);
+
+    const request = await fetch(Global.url + 'user/login', {
+      method: "POST",
+      body: JSON.stringify(userToLogin),
+      headers: {
+        "Content-Type":"application/json"
+      }
+    })
+
+    const data = await request.json();
+  }
+
   return (
     <>
       <header className="content__header content__header--public">
@@ -8,16 +30,16 @@ export const Login = () => {
       </header>
 
       <div className="content__posts">
-        <form className='form-login'>
+        <form className='form-login' onSubmit={loginUser}>
 
           <div className='form-group'>
             <label htmlFor="email">Email</label>
-            <input type="email" name='email' />
+            <input type="email" name='email' onChange={changed}/>
           </div>
 
           <div className='form-group'>
             <label htmlFor="password">Contraseña</label>
-            <input type="password" name='password' />
+            <input type="password" name='password' onChange={changed}/>
           </div>
 
           <input type="submit" value="Identificate" className='btn btn-success' />
