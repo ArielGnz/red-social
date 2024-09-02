a0a818d3101e3e2f14a258fe6863d63b310cbe30
ArielGnz <arielgon2206@gmail.com>
2024-09-02 20:21:46 -0300

    useAuth

--- client/src/context/AuthProvider.jsx
+++ client/src/context/AuthProvider.jsx
@@ -1,7 +1,19 @@
-import React from 'react'
+import React, { createContext } from 'react';
+import { useState, useEffect, createContext } from 'react';
 
-export const AuthProvider = () => {
-  return (
-    <div>AuthProvider</div>
-  )
+
+const AuthContext = createContext();
+
+
+export const AuthProvider = ({ children }) => {
+    
+    const [compartido, setCompartido] = useState("compartido en todos los componentes")
+    
+    return (
+
+        <AuthContext.Provider value={{compartido}}>
+            {children}
+        </AuthContext.Provider>
+    
+    )
 }
--- client/src/router/Routing.jsx
+++ client/src/router/Routing.jsx
@@ -5,31 +5,34 @@ import PrivateLayout from '../components/layout/Private/PrivateLayout';
 import Register from '../components/user/Register';
 import { Login } from '../components/user/Login';
 import { Feed } from '../components/publication/Feed';
+import { AuthProvider } from '../context/AuthProvider';
 
 const Routing = () => {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<PublicLayout />}>
-          <Route index element={<Login />} />
-          <Route path='login' element={<Login />} />
-          <Route path='registro' element={<Register />} />
-        </Route>
+      <AuthProvider>
+        <Routes>
+          <Route path='/' element={<PublicLayout />}>
+            <Route index element={<Login />} />
+            <Route path='login' element={<Login />} />
+            <Route path='registro' element={<Register />} />
+          </Route>
 
-        <Route path='/social' element={<PrivateLayout />}>
-          <Route index element={<Feed />} />
-          <Route path='feed' element={<Feed />} />
-        </Route>
+          <Route path='/social' element={<PrivateLayout />}>
+            <Route index element={<Feed />} />
+            <Route path='feed' element={<Feed />} />
+          </Route>
 
-        <Route path='*' element={
-          <>
-            <p>
-              <h1>Error 404</h1>
-              <Link to='/'>Volver al Inicio</Link>
-            </p>
-          </>
-        } />
-      </Routes>
+          <Route path='*' element={
+            <>
+              <p>
+                <h1>Error 404</h1>
+                <Link to='/'>Volver al Inicio</Link>
+              </p>
+            </>
+          } />
+        </Routes>
+      </AuthProvider>
     </BrowserRouter>
   )
 }
