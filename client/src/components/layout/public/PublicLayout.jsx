4ca9078c62d9e340a9d54cf86dc68eb83cd72c2e
ArielGnz <arielgon2206@gmail.com>
2024-09-06 19:23:44 -0300

    public layout

--- client/src/components/layout/public/PublicLayout.jsx
+++ client/src/components/layout/public/PublicLayout.jsx
@@ -1,15 +1,23 @@
 import React from 'react'
 import { Header } from './Header'
-import { Outlet } from 'react-router-dom'
+import { Navigate, Outlet } from 'react-router-dom';
+import useAuth from '../../../hooks/useAuth';
 
 const PublicLayout = () => {
+
+  const {auth} = useAuth();
+
   return (
     <div>
       
       <Header />
 
       <section className='layout__content'>
-        <Outlet />
+        {!auth._id ? 
+            <Outlet />
+          :
+            <Navigate to="/social" />
+        } 
       </section>
     </div>
   )
