796edc29e867e0e0f8aa337295559c8773c387cb
ArielGnz <arielgon2206@gmail.com>
2024-09-11 20:19:37 -0300

    formulario rellenado con info del user

--- client/src/components/user/Config.jsx
+++ client/src/components/user/Config.jsx
@@ -1,6 +1,10 @@
 import React, { useState } from 'react'
+import useAuth from '../../hooks/useAuth';
+import { Global } from '../../helpers/Global';
 
 export const Config = () => {
+
+    const { auth } = useAuth();
 
     const [saved, setSaved] = useState("not_saved");
 
@@ -23,22 +27,27 @@ export const Config = () => {
 
                     <div className='form-group'>
                         <label htmlFor="name">Nombre</label>
-                        <input type="text" name='name' />
+                        <input type="text" name='name' defaultValue={auth.name} />
                     </div>
 
                     <div className='form-group'>
                         <label htmlFor="surname">Apellido</label>
-                        <input type="text" name='surname' />
+                        <input type="text" name='surname' defaultValue={auth.surname} />
                     </div>
 
                     <div className='form-group'>
                         <label htmlFor="nick">Nick</label>
-                        <input type="text" name='nick' />
+                        <input type="text" name='nick' defaultValue={auth.nick} />
+                    </div>
+
+                    <div className='form-group'>
+                        <label htmlFor="bio">Bio</label>
+                        <textarea name='bio' defaultValue={auth.bio} />
                     </div>
 
                     <div className='form-group'>
                         <label htmlFor="email">Email</label>
-                        <input type="email" name='email' />
+                        <input type="email" name='email' defaultValue={auth.email} />
                     </div>
 
                     <div className='form-group'>
@@ -48,9 +57,11 @@ export const Config = () => {
 
                     <div className='form-group'>
                         <label htmlFor="file0">Avatar</label>
-                        <div className='avatar'>
-
+                        <div className="general-info__container-avatar">
+                            {auth.image != "default.png" && <img src={Global.url + "user/avatar/" + auth.image} className="container-avatar__img" alt="Foto de perfil" />}
+                            {auth.image == "default.png" && <img src={avatar} className="container-avatar__img" alt="Foto de perfil" />}
                         </div>
+                        <br />
                         <input type="file" name='file0' id='file' />
                     </div>
                     <br />
