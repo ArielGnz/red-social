62ee1d412c9d734ac3a4deb2e1e40d7a4836cef4
ArielGnz <arielgon2206@gmail.com>
2024-10-08 20:08:20 -0300

    publicaciones del usuario success, falta paginacion

--- client/src/components/user/Profile.jsx
+++ client/src/components/user/Profile.jsx
@@ -27,9 +27,9 @@ export const Profile = () => {
         getPublications();
     }, [params]);
 
-    const getDataUser = async() => {
+    const getDataUser = async () => {
         let dataUser = await GetProfile(params.userId, setUser);
-        if(dataUser.following && dataUser.following._id) setIFollow(true);
+        if (dataUser.following && dataUser.following._id) setIFollow(true);
     }
 
     const getCounters = async () => {
@@ -63,7 +63,7 @@ export const Profile = () => {
         const data = await request.json();
 
         if (data.status == "success") {
-           setIFollow(true);
+            setIFollow(true);
         }
     }
 
@@ -84,20 +84,22 @@ export const Profile = () => {
         }
     }
 
-    const getPublications = async(nextPage = 1) => {
-        const request = await fetch(Global.url + "publication/user/" + params.userId + "/" + nextPage , {
+    const getPublications = async (nextPage = 1) => {
+        const request = await fetch(Global.url + "publication/user/" + params.userId + "/" + nextPage, {
             method: "GET",
             headers: {
-                "Content-Type" : "application/json",
-                "Authorization" : localStorage.getItem("token")
+                "Content-Type": "application/json",
+                "Authorization": localStorage.getItem("token")
             }
         });
 
         const data = await request.json();
 
-        if(data.status == "success"){
-            setPublications(data);
+        if (data.status == "success") {
+            setPublications(data.publications);
+
         }
+
     }
 
 
@@ -116,10 +118,10 @@ export const Profile = () => {
                         <div className="container-names__name">
                             <h1>{user.name} {user.surname}</h1>
                             {user._id != auth._id &&
-                               
+
                                 (iFollow ?
                                     <button onClick={() => unfollow(user._id)} className="content__button content__button--rigth post__button">DEJAR DE SEGUIR</button>
-                                :
+                                    :
                                     <button onClick={() => follow(user._id)} className="content__button content__button--rigth">SEGUIR</button>
                                 )
                             }
@@ -163,40 +165,49 @@ export const Profile = () => {
 
             <div className="content__posts">
 
-                <article className="posts__post">
+                {publications.map(publication => {
 
-                    <div className="post__container">
+                    return (
 
-                        <div className="post__image-user">
-                            <a href="#" className="post__image-link">
-                                <img src={avatar} className="post__user-image" alt="Foto de perfil" />
-                            </a>
-                        </div>
+                        <article className="posts__post">
 
-                        <div className="post__body">
+                            <div className="post__container">
 
-                            <div className="post__user-info">
-                                <a href="#" className="user-info__name">Victor Robles</a>
-                                <span className="user-info__divider"> | </span>
-                                <a href="#" className="user-info__create-date">Hace 1 hora</a>
+                                <div className="post__image-user">
+                                    <Link to={"/social/perfil/" + publication.user._id} className="post__image-link">
+                                        {publication.user.image != "default.png" && <img src={Global.url + "user/avatar/" + publication.user.image} className="post__user-image" alt="Foto de perfil" />}
+                                        {publication.user.image == "default.png" && <img src={avatar} className="post__user-image" alt="Foto de perfil" />}
+                                        <img src={avatar} className="post__user-image" alt="Foto de perfil" />
+                                    </Link>
+                                </div>
+
+                                <div className="post__body">
+
+                                    <div className="post__user-info">
+                                        <a href="#" className="user-info__name">{publication.user.name + " " + publication.user.surname}</a>
+                                        <span className="user-info__divider"> | </span>
+                                        <a href="#" className="user-info__create-date">{publication.created_at}</a>
+                                    </div>
+
+                                    <h4 className="post__content">{publication.text}</h4>
+
+                                </div>
+
                             </div>
 
-                            <h4 className="post__content">Hola, buenos dias.</h4>
+                            {auth._id == publication.user._id &&
+                                <div className="post__buttons">
 
-                        </div>
+                                    <a href="#" className="post__button">
+                                        <i className="fa-solid fa-trash-can"></i>
+                                    </a>
 
-                    </div>
+                                </div>
+                            }
 
-
-                    <div className="post__buttons">
-
-                        <a href="#" className="post__button">
-                            <i className="fa-solid fa-trash-can"></i>
-                        </a>
-
-                    </div>
-
-                </article>
+                        </article>
+                    )
+                })}
 
             </div>
 
