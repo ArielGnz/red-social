45453fba4d8a17f01dbebb5a6ae2a37bb29dd10d
ArielGnz <arielgon2206@gmail.com>
2024-10-18 11:14:52 -0300

    configurando el main.jsx para el TimeAgo

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -10,6 +10,9 @@ import './assets/css/responsive.css';
 import TimeAgo from "javascript-time-ago";
 import es from "javascript-time-ago/locale/es.json";
 
+TimeAgo.addDefaultLocale(es);
+TimeAgo.addLocale(es);
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   
     <App />
