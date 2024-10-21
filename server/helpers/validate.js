f2f16c6aa9c0fb9c4ec218c2d3c6a8ca36537f85
ArielGnz <arielgon2206@gmail.com>
2024-10-21 18:45:14 -0300

    validaciones

--- server/helpers/validate.js
+++ server/helpers/validate.js
@@ -3,12 +3,20 @@ const validator = require("validator");
 const validate = (params) => {
 
     let name = !validator.isEmpty(params.name) &&
-                validator.isLength(params.name, {min: 3, max: undefined}) &&
-                validator.isAlpha(params.name, "es-ES");
+        validator.isLength(params.name, { min: 3, max: undefined }) &&
+        validator.isAlpha(params.name, "es-ES");
+
+    let surname = !validator.isEmpty(params.surname) &&
+        validator.isLength(params.surname, { min: 3, max: undefined }) &&
+        validator.isAlpha(params.surname, "es-ES");
+
+    let nick = !validator.isEmpty(params.nick) &&
+        validator.isLength(params.nick, { min: 3, max: undefined });
+       
 
 
 
-    if(!name){
+    if (!name) {
         throw new Error("Error en la validacion del Nombre")
     }
 }
