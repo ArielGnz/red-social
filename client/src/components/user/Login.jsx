ac97ef946595c92ccee966641259936f7713feb3
ArielGnz <arielgon2206@gmail.com>
2024-10-24 12:17:01 -0300

    animaciones en el login

--- client/src/components/user/Login.jsx
+++ client/src/components/user/Login.jsx
@@ -90,8 +90,9 @@ export const Login = () => {
           <input type="submit" value="Identificate" className='btn btn-success' />
         </form>
       </div>
-      <div className='hidden lg:flex h-full items-center justify-center bg-gray-200'>
-        <div className='w-60 h-60 bg-gradient-to-tr from-violet-500 to-pink-500 rounded-full'/>
+      <div className='hidden realtive lg:flex h-full w-1/2 items-center justify-center bg-gray-200'>
+        <div className='w-60 h-60 bg-gradient-to-tr from-violet-500 to-pink-500 rounded-full animate-spin'/>
+        <div className='w-full h-[380px] absolute bottom-0 bg-white/10 backdrop-blur-lg'/>
       </div>
 
     </div>
