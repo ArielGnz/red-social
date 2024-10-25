04bb987668fb2249d195f7231a390e2f1af9bd50
ArielGnz <arielgon2206@gmail.com>
2024-10-25 18:19:04 -0300

    trabajando en el navbar del public

--- client/src/components/layout/public/Nav.jsx
+++ client/src/components/layout/public/Nav.jsx
@@ -4,25 +4,47 @@ import { NavLink } from 'react-router-dom'
 
 export const Nav = () => {
 
+    // <nav className="bg-red-500 h-[50px] navbar__container-lists">
+
+    //         <div className="flex justify-between items-center mx-6 container-lists__menu-list">
+    //             <button className="font-medium mx-6 menu-list__item">
+    //                 <NavLink to='/login' className="menu-list__link">
+    //                     {/* <i className="fa-solid fa-user"></i> */}
+    //                     <span className=" text-center menu-list__title">Login</span>
+    //                 </NavLink>
+    //             </button>
+
+    //             <button className="font-medium menu-list__item">
+    //                 <NavLink to='/registro' className="menu-list__link">
+    //                     {/* <i className="fa-solid fa-users"></i> */}
+    //                     <span className="menu-list__title">Register</span>
+    //                 </NavLink>
+    //             </button>
+
+    //         </div>
+
+    //     </nav>
+
     return (
-        <nav className="bg-red-500 h-[50px] navbar__container-lists">
 
-            <div className=" flex justify-between items-center mx-6 container-lists__menu-list">
-                <button className="font-medium mx-6 menu-list__item">
+        <nav className="h-[40px]">
+
+            <ul className='flex justify-between items-center p-2'>
+
+                <li className='mx-8 font-medium'>
                     <NavLink to='/login' className="menu-list__link">
-                        <i className="fa-solid fa-user"></i>
-                        <span className="menu-list__title">Login</span>
+                        <span className=" text-center menu-list__title">Login</span>
                     </NavLink>
-                </button>
 
-                <button className="font-medium menu-list__item">
+                </li>
+
+                <li className='mx-8 font-medium'>
                     <NavLink to='/registro' className="menu-list__link">
-                        <i className="fa-solid fa-users"></i>
                         <span className="menu-list__title">Register</span>
                     </NavLink>
-                </button>
+                </li>
 
-            </div>
+            </ul>
 
         </nav>
     )
