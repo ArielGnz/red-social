5d5028d0e764d996a4856c8fad60cfdca4c5e4e5
ArielGnz <arielgon2206@gmail.com>
2024-10-29 17:48:10 -0300

    trabajando en el nav

--- client/src/components/layout/Private/Nav.jsx
+++ client/src/components/layout/Private/Nav.jsx
@@ -103,29 +103,29 @@ export const Nav = () => {
         //         </ul>
 
         //         <ul className="flex space-x-4">
-        //             <li className="font-medium text-gray-200 hover:text-white">
-        //                 <NavLink to={"/social/perfil/" + auth._id}>
-        //                     {/* {auth.image != "default.png" && <img src={Global.url + "user/avatar/" + auth.image} className="list-end__img" alt="Foto de perfil" />}
-        //                     {auth.image == "default.png" && <img src={avatar} className="list-end__img" alt="Foto de perfil" />} */}
-        //                 </NavLink>
-        //             </li>
-        //             <li className="font-medium text-gray-200 hover:text-white">
-        //                 <NavLink to={"/social/perfil/" + auth._id}>
-        //                     <span>{auth.nick}</span>
-        //                 </NavLink>
-        //             </li>
-        //             <li className="font-medium text-gray-200 hover:text-white">
-        //                 <NavLink to="/social/ajustes" >
+        // <li className="font-medium text-gray-200 hover:text-white">
+        //     <NavLink to={"/social/perfil/" + auth._id}>
+        //         {/* {auth.image != "default.png" && <img src={Global.url + "user/avatar/" + auth.image} className="list-end__img" alt="Foto de perfil" />}
+        //         {auth.image == "default.png" && <img src={avatar} className="list-end__img" alt="Foto de perfil" />} */}
+        //     </NavLink>
+        // </li>
+        // <li className="font-medium text-gray-200 hover:text-white">
+        //     <NavLink to={"/social/perfil/" + auth._id}>
+        //         <span>{auth.nick}</span>
+        //     </NavLink>
+        // </li>
+        // <li className="font-medium text-gray-200 hover:text-white">
+        //     <NavLink to="/social/ajustes" >
 
-        //                     <span>Ajustes</span>
-        //                 </NavLink>
-        //             </li>
-        //             <li className="font-medium text-gray-200 hover:text-white">
-        //                 <NavLink to="/social/logout" >
+        //         <span>Ajustes</span>
+        //     </NavLink>
+        // </li>
+        // <li className="font-medium text-gray-200 hover:text-white">
+        //     <NavLink to="/social/logout" >
 
-        //                     <span>Cerrar Sesion</span>
-        //                 </NavLink>
-        //             </li>
+        //         <span>Cerrar Sesion</span>
+        //     </NavLink>
+        // </li>
         //         </ul>
         //     </div>
 
@@ -145,18 +145,61 @@ export const Nav = () => {
 
         // </nav>
 
-        <nav className="flex justify-between py-6 m-auto px-10 text-lg font-medium tracking-widest md:items-center lg:w-[85%]">
-            <div><h1 className='font-serif italic text-3xl text-white bg-slate-700 rounded-full border-4 border-sky-500 p-2 ml-4 hover:text-sky-400'>AG</h1></div>
-            <div id="menu" className={`p-5 h-[220px] w-[220px] flex flex-col items-center lg:justify-end rounded-lg  
-                    text-white md:flex-row md:w-full md:py-0 md:h-[50px] ${menuVisible ? 'flex' : 'hidden'} md:flex `}>
+        <nav className="flex justify-between py-4 m-auto px-4 text-lg font-medium tracking-widest md:items-center lg:w-full">
+            
+            <div><h1 className='mx-4 font-medium text-gray-200 hover:text-white'>REACTSOCIAL</h1></div>
+            
+            <div id="menu" className={`h-[220px] w-[220px] flex flex-col items-center lg:justify-end rounded-lg  
+                     md:flex-row md:w-full md:py-0 md:h-[50px] ${menuVisible ? 'flex' : 'hidden'} md:flex `}>
+                
                 <ul className='flex h-5/6 flex-col justify-around md:flex-row md:justify-end w-full '>
-                    <li className='md:px-8 hover:text-sky-600'><a href="#inicio"> Inicio </a></li>
-                    <li className='md:px-8 hover:text-sky-600'><a href="#sobreMi"> Quien Soy </a></li>
-                    <li className='md:px-8 hover:text-sky-600'><a href="#proyectos"> Proyectos </a></li>
-                    <li className='md:px-8 hover:text-sky-600'><a href="#contacto"> Contactame </a></li>
+                    
+                    <li className="font-medium px-2 text-gray-200 hover:text-white">
+                        <NavLink to="/social">
+                            <span>Inicio</span>
+                        </NavLink>
+                    </li>
+
+                    <li className="font-medium px-2 text-gray-200 hover:text-white">
+                        <NavLink to="/social/feed">
+                            <span>Timeline</span>
+                        </NavLink>
+                    </li>
+
+                    <li className="font-medium px-2 text-gray-200 hover:text-white">
+                        <NavLink to="/social/gente">
+                            <span>Gente</span>
+                        </NavLink>
+                    </li>
+
+                    <li className="font-medium px-2 text-gray-200 hover:text-white">
+                        <NavLink to={"/social/perfil/" + auth._id}>
+                            {/* {auth.image != "default.png" && <img src={Global.url + "user/avatar/" + auth.image} className="list-end__img" alt="Foto de perfil" />}
+                            {auth.image == "default.png" && <img src={avatar} className="list-end__img" alt="Foto de perfil" />} */}
+                        </NavLink>
+                    </li>
+                    <li className="font-medium px-2 text-gray-200 hover:text-white">
+                        <NavLink to={"/social/perfil/" + auth._id}>
+                            <span>{auth.nick}</span>
+                        </NavLink>
+                    </li>
+                    <li className="font-medium px-2 text-gray-200 hover:text-white">
+                        <NavLink to="/social/ajustes" >
+
+                            <span>Ajustes</span>
+                        </NavLink>
+                    </li>
+                    <li className="font-medium px-2 text-gray-200 hover:text-white">
+                        <NavLink to="/social/logout" >
+
+                            <span>Cerrar Sesion</span>
+                        </NavLink>
+                    </li>
                 </ul>
+
             </div>
-            <button id="menu-button" className="absolute top-8 right-12 md:hidden" onClick={toggleMenu}>
+
+            <button id="menu-button" className="absolute right-8 top-2 md:hidden" onClick={toggleMenu}>
                 <span
                     className="br-1 block bg-white my-2 rounded-md h-[3px] w-[28px]"
                 ></span>
