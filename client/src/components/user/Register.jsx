daa786eb57621e59defaa139cc5c2185ca0531fc
ArielGnz <arielgon2206@gmail.com>
2024-11-11 20:01:44 -0300

    limpiando el codigo

--- client/src/components/user/Register.jsx
+++ client/src/components/user/Register.jsx
@@ -2,7 +2,8 @@ import React, { useState } from 'react'
 import { useForm } from '../../hooks/useForm';
 import { Global } from '../../helpers/Global';
 import fondo10 from "../../assets/img/fondo10.png";
-import logo1 from "../../assets/img/logo1.png"
+import logo1 from "../../assets/img/logo1.png";
+import { Navigate } from 'react-router-dom';
 
 export default function Register() {
 
@@ -33,49 +34,7 @@ export default function Register() {
 
     }
 
-    // <>
-    //     <header className="content__header content__header--public">
-    //         <h1 className="content__title">Registro</h1>
-    //     </header>
-
-    //     <div className="content__posts">
-
-    //         {saved == "saved" ? <strong className='alert alert-success'> "Usuario registrado correctamente!!" </strong> : " "}
-    //         {saved == "Error" ? <strong className='alert alert-danger'> "Error al registrar el usuario" </strong> : " "}
-
-    //         <form className='register-form' onSubmit={saveUser}>
-
-    //             <div className='form-group'>
-    //                 <label htmlFor="name">Nombre</label>
-    //                 <input type="text" name='name' onChange={changed} />
-    //             </div>
-
-    //             <div className='form-group'>
-    //                 <label htmlFor="surname">Apellido</label>
-    //                 <input type="text" name='surname' onChange={changed} />
-    //             </div>
-
-    //             <div className='form-group'>
-    //                 <label htmlFor="nick">Nick</label>
-    //                 <input type="text" name='nick' onChange={changed} />
-    //             </div>
-
-    //             <div className='form-group'>
-    //                 <label htmlFor="email">Email</label>
-    //                 <input type="email" name='email' onChange={changed} />
-    //             </div>
-
-    //             <div className='form-group'>
-    //                 <label htmlFor="password">Contraseña</label>
-    //                 <input type="password" name='password' onChange={changed} />
-    //             </div>
-
-    //             <input type="submit" value="Registrate" className='btn btn-success' />
-
-    //         </form>
-    //     </div>
-    // </>
-
+    
     return (
 
         <div className='flex w-full h-screen '>
